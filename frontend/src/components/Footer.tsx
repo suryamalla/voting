96503ac734ab3fc1bfce73769cbd4e14a28da661
surya <suryamalla55@gmail.com>
2024-08-04 10:31:05 +0545

    fix: fix

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ paddingBottom: 20, paddingTop: 50, textAlign: "center" }}
+      className="text-normal"
+    >
+      <div>Copyright © 2024 Hasina Khatiwada. All rights reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
